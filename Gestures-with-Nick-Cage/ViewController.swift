876191b17be72bd5f50598869f82b7c104ee1938
Alvin <alvintu@Alvins-MacBook-Pro.local>
2018-10-25 12:48:28 -0400

    Initial Commit

---
+++ Gestures-with-Nick-Cage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gestures-with-Nick-Cage
+//
+//  Created by Alvin on 10/25/18.
+//  Copyright © 2018 AlvinTu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Gestures-with-Nick-CageTests/Gestures_with_Nick_CageTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gestures_with_Nick_CageTests.swift
+//  Gestures-with-Nick-CageTests
+//
+//  Created by Alvin on 10/25/18.
+//  Copyright © 2018 AlvinTu. All rights reserved.
+//
+
+import XCTest
+@testable import Gestures_with_Nick_Cage
+
+class Gestures_with_Nick_CageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
